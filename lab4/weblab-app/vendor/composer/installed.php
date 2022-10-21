d0c73e98e7f0fdf41fc5da61710a957f0187e4f0
Aleksandr Pisklov <sasha.pisklov2002@gmail.com>
2022-10-21 22:47:53 +0300

    add weblab-app

---
+++ lab4/weblab-app/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'aleksandr/weblab-app',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aleksandr/weblab-app' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'aleksandr/weblab-lib' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8e7498a3e282d61d4fda95c4c3f739c9a8df5763',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../aleksandr/weblab-lib',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
